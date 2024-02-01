d50fa30be4068034becb335f9ae60a84a14070af
Daniel Aguiar <dc03934@gmail.com>
2024-02-01 09:20:34 -0300

    Alteraçoes realiazadas

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,12 +8,48 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  title = 'Harvey Specter';
+  title = 'Daniel Aguiar';
   
   constructor() {
     this.title = this.title.replace(/(\w+)\s(\w+)/, '$1');
+  }
+
+  ngOnInit() {
+    // const title = document.querySelector('.title');
+    // title && (title.innerHTML = this.title);
+  }
+
+  public prepareRoute(outlet: RouterOutlet): void {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  public navigation(event: any): void {
+    event.preventDefault();
+
+    const item = event.target.closest('a[routerLink^="#"]');
+    const target = event.target.getAttribute('routerLink');
+
+    if (item && target) {
+      const targetElement = document.querySelector(target);
+      targetElement && targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  public functions(element: any): void {
+    const textArray = element.innerHtml.split('');
+    element.innerHtml = '';
+    textArray.forEach((letter: string, index: number) => {
+      setTimeout(() => {
+        element.innerHtml += letter;
+      }, 75 * index);
+    });
+  }
+
+  public scroll(): void {
+    const element = document.querySelector('#about');
+    element && element.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
 }
